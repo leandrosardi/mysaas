55a05e642b6ac7614f8317af6443db12ee232f81
DEV1\Leandro <leandro.sardi@expandedventure.com>
2022-06-07 13:56:04 -0300

    #2

--- lib/extensions.rb
+++ lib/extensions.rb
@@ -2,25 +2,57 @@ require 'blackstack-deployer'
 
 module BlackStack
     @@path
-  
+    @@dependencies = []
+    @@app = {}
+    @@setting_screens = []
+
     # setters and getters
     def self.set_path(s)
         @@path = s
     end
-    def path()
+    def self.path()
         @@path
+    end
+    def self.dependencies()
+        @@dependencies
+    end
+    def self.app()
+        @@app
+    end
+    def self.setting_screens()
+        @@setting_screens
     end
 
     # return an array of string with the errors found in the extension descriptor
     def self.validate_descriptor(h)
-        name
-        description
-        folder
-        version
+        errors = []
 
+        # validate: the name is required
+        errors << "The name is required" if h['name'].nil?
+        # validate: the name is a string
+        errors << "The name must be a string" unless h['name'].is_a? String
+
+        # validate: the description is required
+        errors << "The description is required" if h['description'].nil?
+        # validate: the description is a string
+        errors << "The description must be a string" unless h['description'].is_a? String
+
+        # validate: the folder is required
+        errors << "The folder is required" if h['folder'].nil?
+        # validate: the folder is a valid filename
+        errors << "The folder must be a valid filename" unless h['folder'].match(/^[a-zA-Z0-9_\-]+$/) # TODO: mover esta validacion a blackstack-core
+
+        # validate: the version is required
+        errors << "The version is required" if h['version'].nil?
+        # validate: the version is a version number
+        errors << "The version must be a valid version number" unless h['version'].match(/^\d+\.\d+\.\d+$/) # TODO: mover esta validacion a blackstack-core
+
+=begin
         dependencies
         app
-        
+        settings
+        storage_folders
+=end
     end
 
     def self.appened(:helpdesk)
