3ef9f725dd8f1b443c24420a2ad0c8d5d3d135b9
leandrosardi <leandro.sardi@expandedventure.com>
2022-06-09 15:47:41 -0300

    #2

--- deployment-routines/install-crdb-database.rb
+++ deployment-routines/install-crdb-database.rb
@@ -5,22 +5,22 @@ BlackStack::Deployer::add_routine({
     { 
         :command => "cockroach sql --host %eth0_ip%:%crdb_database_port% --certs-dir %crdb_database_certs_path%/certs -e \"CREATE USER blackstack WITH PASSWORD %crdb_database_password%;\"",
         :matches => [/CREATE ROLE/, /already exists/],
-        :sudo => true,
+        :sudo => false,
     }, {
         :command => "cockroach sql --host %eth0_ip%:%crdb_database_port% --certs-dir %crdb_database_certs_path%/certs -e \"CREATE DATABASE blackstack 
 WITH stat
 ENCODING = UTF8 
 CONNECTION LIMIT = -1;\"",
         :matches => [/CREATE DATABASE/, /already exists/],
-        :sudo => true,
+        :sudo => false,
     }, {
         :command => "cockroach sql --host %eth0_ip%:%crdb_database_port% --certs-dir %crdb_database_certs_path%/certs -e \"GRANT ALL ON DATABASE blackstack TO blackstack;\"",
         :matches => [/GRANT/, /unexpected end of file/],
-        :sudo => true,
+        :sudo => false,
     }, {
         :command => "cockroach sql --host %eth0_ip%:%crdb_database_port% --certs-dir %crdb_database_certs_path%/certs -e \"SHOW GRANTS ON DATABASE blackstack;\"",
         :matches => [/database_name\.*|\.*grantee\.*|\.*privilege_type/, /unexpected end of file/],
-        :sudo => true,
+        :sudo => false,
     }
   ],
 });
